75cb0b5261cbf022cb02ed6aead4dadc540d5c73
vladimir escalante <vladimirescalante75@gmail.com>
2023-11-03 19:47:08 -0300

    readme terminado

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
-    <CssBaseline /> {/* //reset de css basico */}
+    <CssBaseline />
     <App />
   </>
 );
